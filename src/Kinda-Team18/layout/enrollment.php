302cc8a79dcd39be44ff769636fbff16496dd317
nedaMaghrebi <neda22000@student.hamk.fi>
2023-02-18 15:55:22 +0200

    create a foolder layout

---
+++ src/Kinda-Team18/layout/enrollment.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<?php include "headerr.php" ?>
+<div class="row">
+<img class="header-image" src="https://th.bing.com/th/id/OIP.UeFNYlBO1xM03hoOFjU0mQAAAA?pid=ImgDet&rs=1 " alt="background image"class="rounded" width="500" height="400">>
+</div>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+
+    <div class="row">
+        <div class="col-md-12">
+        <h4><b><i>ADMISSION OPEN FOR 2023</i></b></h4>
+        </div>
+    </div>  
+    <div class="row">
+        <div class="col-md-12 text-center">
+        <h5><b> Download our enrollment form and email to</b></h5>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+        <h6><b> info@twinklingstars.fi</b></h6>  
+    </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+        <a class="btn btn-danger" href="page 5" role="button">Enrollment form</a>
+        </div>
+    
+    
+</div>
+
+
+<?php include "footer1.php" ?>
